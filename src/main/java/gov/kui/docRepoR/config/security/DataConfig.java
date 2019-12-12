aa97c8204a14e84356f9a9d246f3d8e93e912c96
Serg N <serg_vn@inbox.ru>
2019-12-12 15:55:53 +0900

    adding CorsFilter & DataConfig(with userDetailService)

---
+++ src/main/java/gov/kui/docRepoR/config/security/DataConfig.java
@@ -0,0 +1,19 @@
+package gov.kui.docRepoR.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class DataConfig {
+    @Bean
+    public UserDetailsService userDetailsService() {
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(users.username("user").password("user").roles("USER").build());
+        manager.createUser(users.username("admin").password("admin").roles("ADMIN").build());
+        return manager;
+    }
+}
--- src/main/java/gov/kui/docRepoR/config/security/SecurityConfig.java
+++ src/main/java/gov/kui/docRepoR/config/security/SecurityConfig.java
@@ -1,40 +1,35 @@
 package gov.kui.docRepoR.config.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-        @Autowired
-        private UserDetailsService userDetailsService;
-        @Bean
-        public UserDetailsService userDetailsService() {
-            User.UserBuilder users = User.withDefaultPasswordEncoder();
-            InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-            manager.createUser(users.username("user").password("user").roles("USER").build());
-            manager.createUser(users.username("admin").password("admin").roles("ADMIN").build());
-            return manager;
-        }
+    UserDetailsService userDetailsService;
+
+    @Autowired
+    public void setUserDetailsService(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
+        http.addFilterBefore(new CorsFilter(), ChannelProcessingFilter.class);
+
         http.authorizeRequests()
                 .antMatchers("/api/**").access("hasRole('ADMIN')")
                 .antMatchers("/api/**").access("hasRole('USER')")
-               // .antMatchers("/").permitAll()
-                .and().csrf().disable().cors().disable()
-                .formLogin()
-                .defaultSuccessUrl("/", false);
+                .and().csrf().disable()
+                .httpBasic()
+                .and()
+                .formLogin();
     }
 }
 
