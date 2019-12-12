ea0a146f5799d3038b283977c7f1418264dc2f3a
Serg N <serg_vn@inbox.ru>
2019-12-12 14:42:34 +0900

    start inMemory security

--- src/main/java/gov/kui/docRepoR/config/security/SecurityConfig.java
+++ src/main/java/gov/kui/docRepoR/config/security/SecurityConfig.java
@@ -3,7 +3,6 @@ package gov.kui.docRepoR.config.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,21 +10,10 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    /*
-    private DataSource securityDataSource;
-
-    @Autowired
-    public SecurityConfig(DataSource securityDataSource) {
-        this.securityDataSource = securityDataSource;
-    }
-
-     */
-
 
         @Autowired
         private UserDetailsService userDetailsService;
@@ -40,15 +28,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        /*
-        http.authorizeRequests()
-                .antMatchers("/api/**").access("hasRole('ADMIN')")
-                .antMatchers("/api/**").access("hasRole('EMPLOYEE')")
-                .and().csrf().disable()
-//                .cors().disable()
-                .formLogin()
-                .defaultSuccessUrl("/", false);
-         */
         http.authorizeRequests()
                 .antMatchers("/api/**").access("hasRole('ADMIN')")
                 .antMatchers("/api/**").access("hasRole('USER')")
@@ -57,12 +36,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .defaultSuccessUrl("/", false);
     }
-/*
-    @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication().dataSource(securityDataSource);
-    }
-
- */
 }
 
