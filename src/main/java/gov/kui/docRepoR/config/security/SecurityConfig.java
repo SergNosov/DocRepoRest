dfff606ae8de24ec9d9531fe519d343efd156cf1
Serg N <serg_vn@inbox.ru>
2019-12-12 16:07:11 +0900

    adding CorsFilter

--- src/main/java/gov/kui/docRepoR/config/security/SecurityConfig.java
+++ src/main/java/gov/kui/docRepoR/config/security/SecurityConfig.java
@@ -1,15 +1,12 @@
 package gov.kui.docRepoR.config.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 
 import javax.sql.DataSource;
 
@@ -26,13 +23,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
 
+        http.addFilterBefore(new CorsFilter(), ChannelProcessingFilter.class);
+
         http.authorizeRequests()
                 .antMatchers("/api/**").access("hasRole('ADMIN')")
                 .antMatchers("/api/**").access("hasRole('EMPLOYEE')")
                 .and().csrf().disable()
                 .cors().disable()
                 .formLogin()
-                .defaultSuccessUrl("/", false);
+                .and().httpBasic();
     }
 
     @Override
