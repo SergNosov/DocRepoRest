982f12193acb69d9d4553e2ae7aaa759e577d5a7
Serg N <serg_vn@inbox.ru>
2019-12-17 17:12:20 +0900

    adding  handleBadCredentialsException in RestExceptionHandler

--- src/main/java/gov/kui/docRepoR/controller/RestExceptionHandler.java
+++ src/main/java/gov/kui/docRepoR/controller/RestExceptionHandler.java
@@ -1,16 +1,24 @@
 package gov.kui.docRepoR.controller;
 
+import gov.kui.docRepoR.model.ApiResponse;
 import gov.kui.docRepoR.model.CommonMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class RestExceptionHandler {
-    @ExceptionHandler
-    public ResponseEntity<CommonMessage> handleException(Exception ex){
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<CommonMessage> handleAllException(Exception ex){
         CommonMessage commonMessage = new CommonMessage(ex.toString()+ " : "+ex.getMessage());
         return new ResponseEntity<CommonMessage>(commonMessage, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<CommonMessage> handleBadCredentialsException(BadCredentialsException ex){
+        CommonMessage commonMessage = new CommonMessage(ex.toString()+ " : "+ex.getMessage());
+        return new ResponseEntity<CommonMessage>(commonMessage, HttpStatus.UNAUTHORIZED);
     }
 }
