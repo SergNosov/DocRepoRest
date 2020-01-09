4554e58d876db719610c07b1f8838af57f60f08d
Serg N <serg_vn@inbox.ru>
2020-01-09 13:14:10 +0900

    refactoring Document entity

--- src/main/java/gov/kui/docRepoR/model/Document.java
+++ src/main/java/gov/kui/docRepoR/model/Document.java
@@ -1,11 +1,7 @@
 package gov.kui.docRepoR.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -13,15 +9,12 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 @Entity
 @Table(name = "document")
@@ -56,14 +49,6 @@ public class Document implements DocRepoEntity {
             joinColumns = @JoinColumn(name = "document_id"),
             inverseJoinColumns = @JoinColumn(name = "sender_id"))
     private List<Sender> senders = new ArrayList<>();
-
-    /*
-    @OneToMany(orphanRemoval = true,
-            fetch = FetchType.LAZY,
-            cascade = {CascadeType.PERSIST, CascadeType.MERGE})
-    @JoinColumn(name = "document_id")
-    private Set<FileEntity> files = new HashSet<>();
-     */
 
     public Document() {
     }
@@ -138,18 +123,6 @@ public class Document implements DocRepoEntity {
         }
     }
 
-    /*
-    public Set<FileEntity> getFiles() {
-        return files;
-    }
-
-    public void setFiles(Set<FileEntity> files) {
-        if (files != null) {
-            this.files = files;
-        }
-    }
-     */
-
     @Override
     public String toString() {
         return "Document{" +
@@ -160,7 +133,6 @@ public class Document implements DocRepoEntity {
                 ",\n content='" + content + '\'' +
                 ",\n doctype=" + doctype +
                 ",\n senders=" + senders +
-        //        ",\n files=" + files +
                 '}';
     }
 }
