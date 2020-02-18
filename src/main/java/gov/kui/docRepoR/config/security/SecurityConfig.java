8e4a4de3db1aee36f54347ae4b17dcdbfc9d0ecc
Serg N <serg_vn@inbox.ru>
2020-02-18 17:16:28 +0900

    adding https in application.properties

--- src/main/java/gov/kui/docRepoR/config/security/SecurityConfig.java
+++ src/main/java/gov/kui/docRepoR/config/security/SecurityConfig.java
@@ -60,5 +60,38 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
+
+
+    /*
+    // если нужен редирект с HTTP на HTTPS (optional)
+
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+
+        tomcat.addAdditionalTomcatConnectors(initiateHttpConnector());
+        return tomcat;
+    }
+
+    private Connector initiateHttpConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(8443);
+
+        return connector;
+    }
+     */
 }
 
--- src/main/java/gov/kui/docRepoR/domain/Constants.java
+++ src/main/java/gov/kui/docRepoR/domain/Constants.java
@@ -3,6 +3,6 @@ package gov.kui.docRepoR.domain;
 public class Constants {
     public static final long ACCESS_TOKEN_VALIDITY_SECONDS = 30*60;//30 min = 1 800 sec
     public static final String SIGNING_KEY = "kuiChita123r!";
-    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String TOKEN_PREFIX = "Bearer";
     public static final String HEADER_STRING = "Authorization";
 }
