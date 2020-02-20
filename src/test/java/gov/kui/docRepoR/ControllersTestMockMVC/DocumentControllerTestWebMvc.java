39f4243318a02cf4ff03b589ebd8be0d726c469c
Serg N <serg_vn@inbox.ru>
2020-02-20 17:43:35 +0900

    changing DocumentControllerTestWebMvc on SpringBootTest

--- src/test/java/gov/kui/docRepoR/ControllersTestMockMVC/DocumentControllerTestWebMvc.java
+++ src/test/java/gov/kui/docRepoR/ControllersTestMockMVC/DocumentControllerTestWebMvc.java
@@ -2,64 +2,47 @@ package gov.kui.docRepoR.ControllersTestMockMVC;
 
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import gov.kui.docRepoR.DocRepoURL;
-import gov.kui.docRepoR.config.security.JwtAuthenticationEntryPoint;
-import gov.kui.docRepoR.config.security.JwtTokenUtil;
-import gov.kui.docRepoR.controller.DocumentController;
 import gov.kui.docRepoR.domain.Document;
 import gov.kui.docRepoR.JsonDocument;
 import gov.kui.docRepoR.service.DocumentService;
-import gov.kui.docRepoR.service.FileEntityService;
-import gov.kui.docRepoR.service.UserService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-import javax.sql.DataSource;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 import java.io.IOException;
+
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(DocumentController.class)
+@SpringBootTest
 public class DocumentControllerTestWebMvc {
 
-    @MockBean
-    FileEntityService fileEntityService;
+    @Autowired
+    private WebApplicationContext wac;
 
-    @MockBean
-    JwtAuthenticationEntryPoint unauthorizedHandler;
-
-    @MockBean
-    DataSource securityDataSource;
-
-    @MockBean
-    private JwtTokenUtil jwtTokenUtil;
-
-    @MockBean
-    UserService userService;
+    private MockMvc mockMvc;
+    private Document validDocument;
 
     @MockBean
     private DocumentService documentService;
 
-    @Autowired
-    private MockMvc mockMvc;
-
-    private Document validDocument;
-
     @BeforeEach
     void setUp() throws IOException {
         validDocument = new ObjectMapper().registerModule(new JavaTimeModule())
-                .readValue(JsonDocument.JSON_GOOD_2_SENDERS.toString(), Document.class);
+                .readValue(JsonDocument.JSON_GOOD.toString(), Document.class);
+
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).dispatchOptions(true).build();
     }
 
     @AfterEach
@@ -71,11 +54,10 @@ public class DocumentControllerTestWebMvc {
     void testGetDocumentById() throws Exception {
         when(documentService.findById(anyInt())).thenReturn(validDocument);
 
-        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get(
-                DocRepoURL.DOCUMENTS_LOCALHOST.toString() + "/" + validDocument.getId());
-        requestBuilder.contentType(MediaType.APPLICATION_JSON_UTF8);
-
-        mockMvc.perform(requestBuilder)
+        mockMvc.perform(get(DocRepoURL.DOCUMENTS_LOCALHOST.toString() + "/1000")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+              //  .with(SecurityRequestPostProcessors.userDetailsService("john"))
+        )
                 .andExpect(status().isOk())
                 .andDo(print());
     }
