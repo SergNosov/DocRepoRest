e6bca9b9fecfc7681b873f38cb3237c1095e7cf0
Serg N <serg_vn@inbox.ru>
2020-02-20 11:57:43 +0900

    show ApplicationContext

--- src/main/java/gov/kui/docRepoR/DocRepoRApplication.java
+++ src/main/java/gov/kui/docRepoR/DocRepoRApplication.java
@@ -2,10 +2,15 @@ package gov.kui.docRepoR;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DocRepoRApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(DocRepoRApplication.class, args);
+		ApplicationContext context = SpringApplication.run(DocRepoRApplication.class, args);
+
+		for(String beanName: context.getBeanDefinitionNames()){
+			System.out.println("beanName: "+beanName);
+		}
 	}
 }
