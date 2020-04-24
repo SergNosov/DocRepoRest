5bb68d8bdcc2dd816a080e4924df0a4bfcd7a408
SVN <serg_vn@inbox.ru>
2020-04-24 23:17:34 +1000

    create FileEntityRepositoryDataJpaTest

---
+++ src/test/java/gov/kui/docRepoR/dao/FileEntityRepositoryDataJpaTest.java
@@ -0,0 +1,21 @@
+package gov.kui.docRepoR.dao;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@Slf4j
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class FileEntityRepositoryDataJpaTest {
+
+    @Autowired
+    private FileEntityRepository fileEntityRepository;
+
+    @Test
+    void findDtoByIdTestOk(){
+
+    }
+}
