6f34ec11863ebb5a0d130868f35bff33c6245375
SVN <serg_vn@inbox.ru>
2020-05-14 22:11:14 +1000

    refactoring  DocumentRepositoryDataJpaTest (create query)

--- src/test/java/gov/kui/docRepoR/dao/DocumentRepositoryDataJpaTest.java
+++ src/test/java/gov/kui/docRepoR/dao/DocumentRepositoryDataJpaTest.java
@@ -1,9 +1,10 @@
 package gov.kui.docRepoR.dao;
 
-import gov.kui.docRepoR.domain.Document;
 import gov.kui.docRepoR.dto.DoctypeDto;
 import gov.kui.docRepoR.dto.DocumentDto;
+import gov.kui.docRepoR.dto.SenderDto;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.query.NativeQuery;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -11,6 +12,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
+import java.util.List;
 
 @Slf4j
 @DataJpaTest
@@ -22,23 +24,32 @@ public class DocumentRepositoryDataJpaTest {
 
     @Test
     void testEm() {
+        final int documentId = 2;
+
         TypedQuery<DocumentDto> query = entityManager.createQuery(
                 "select new gov.kui.docRepoR.dto.DocumentDto(d.id,d.number,d.docDate,d.title,d.content)" +
                         " from Document d where d.id = :id",
                 DocumentDto.class)
-                .setParameter("id", 21);
-
+                .setParameter("id", documentId);
         DocumentDto documentDto = query.getSingleResult();
 
         TypedQuery<DoctypeDto> doctypeDtoTypedQuery = entityManager.createQuery(
                 "select new gov.kui.docRepoR.dto.DoctypeDto(d.id,d.title) from Doctype d" +
-                        " inner join Document doc on d.id = doc.doctype.id ",DoctypeDto.class
-       //                 " inner join Document doc on d.id = doc.doctype.id ",DoctypeDto.class
-        );
-
+                        " join Document doc on d.id = doc.doctype.id where doc.id = :id",
+                DoctypeDto.class)
+                .setParameter("id", documentId);
         DoctypeDto doctypeDto = doctypeDtoTypedQuery.getSingleResult();
 
+        NativeQuery<SenderDto> nativeQuery = (NativeQuery<SenderDto>) entityManager.createNativeQuery("select s.id, s.title from sender s"+
+                " inner join document_sender ds on s.id = ds.sender_id "+
+                " inner join document doc on doc.id = ds.document_id where doc.id = :idDoc"
+                ).setParameter("idDoc", documentId);
+
+        List<SenderDto> senderDtos = nativeQuery.getResultList();
+
+
         documentDto.setDoctype(doctypeDto);
+        documentDto.setSenders(senderDtos);
 
         log.info("------ " + documentDto.toString());
     }
