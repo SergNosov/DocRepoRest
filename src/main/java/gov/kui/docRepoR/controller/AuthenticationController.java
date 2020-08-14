c330c556ac11002e36fd2d90c9bf1102d2f334c2
SVN <serg_vn@inbox.ru>
2020-08-14 18:15:41 +1000

    refactor AuthenticationController

--- src/main/java/gov/kui/docRepoR/controller/AuthenticationController.java
+++ src/main/java/gov/kui/docRepoR/controller/AuthenticationController.java
@@ -4,7 +4,6 @@ import gov.kui.docRepoR.config.security.JwtTokenUtil;
 import gov.kui.docRepoR.domain.ApiResponse;
 import gov.kui.docRepoR.domain.AuthToken;
 import gov.kui.docRepoR.domain.DocRepoUser;
-import gov.kui.docRepoR.domain.CommonMessage;
 import gov.kui.docRepoR.domain.LoginUser;
 import gov.kui.docRepoR.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -78,23 +77,4 @@ public class AuthenticationController {
                 "success",
                 new AuthToken(docRepoUser.getId(),docRepoUser.getUsername(),authToken));
     }
-
-//    @GetMapping("/login/check")
-//    public ApiResponse<AuthToken> checkToken(HttpServletRequest request){
-//
-//        final String header = request.getHeader(AUTH_HEADER_STRING);
-//
-//        if (header==null || !header.startsWith(TOKEN_PREFIX)) {
-//            throw new BadCredentialsException("AuthenticationController: checkToken() is failed.");
-//        }
-//
-//        final String authToken = header.replace(TOKEN_PREFIX, "");
-//        final String username = jwtTokenUtil.getUsernameFromToken(authToken);
-//        final DocRepoUser docRepoUser = userService.findByUsername(username);
-//
-//        return new ApiResponse<AuthToken>(HttpStatus.OK.value(),
-//                HttpStatus.OK.toString(),
-//                "success",
-//                new AuthToken(docRepoUser.getId(),docRepoUser.getUsername(),authToken));
-//    }
 }
